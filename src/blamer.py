b70b1a8def258b5de1f3a17eab8053ce5059d0ee
hsouza2001 <henrique.eduardo@virtualisurg.com>
2025-10-19 21:36:11 -0300

    feat: improve blamer structure

--- src/blamer.py
+++ src/blamer.py
@@ -1,71 +1,49 @@
 from ingestor import Ingestor
 from llm import LLM
 from langchain_core.prompts import PromptTemplate
+from requirement import Requirement
+from file_manager import FileManager
 import json
 import re
+from pathlib import Path
+from typing import *
 
 class Blamer:
-    def __init__(self):
-        self.requirement = None
-        self.exam_text = None
-        self.prompt_template = None
-        self.prompt = None
-        self.answer = None
-        self.answer_json = None
+    def __init__(self, prompt_template_path: Path, model: str):
+        self.prompt_template_path: Path = prompt_template_path
+        self.model: str = model
+        self.prompt_template: str = None
+        self.prompt: str = None
+        self.load_prompt_template()
         return
     
     
-    def blame(self):
-        llm = LLM(model='gemma3:12b')
-        self.answer = llm.ask(self.prompt)
-        return
-    
-    
-    def parse_answer(self):
-        self.answer_json = AnswerParser.extract_json_from_string(self.answer)
-        return
-    
-    
-    def load_requirement(self, req_file_path: str):
-        self.requirement = self._load_doc(req_file_path)
-        return
-    
-    
-    def load_exam(self, exam_text: str):
-        self.exam_text = exam_text
-        return
-        
-    
-    def load_prompt_template(self, prompt_template_file_path: str):
-        self.prompt_template = self._load_doc(prompt_template_file_path)
-        return
+    def blame(self, exam_text: str, requirement: Union[str, Requirement]):
+        self.compile_prompt(exam_text=exam_text, requirement=requirement)
+        llm = LLM(model=self.model)
+        answer = llm.ask(self.prompt)
+        print(answer)
+        answer = LLM.extract_json_from_answer(answer)
+        return answer
 
+
+    def load_prompt_template(self):
+        self.prompt_template = FileManager.load_text(self.prompt_template_path)
+        return
     
-    def generate_prompt(self):
+    
+    def compile_prompt(self, exam_text: str, requirement: Union[str, Requirement]):
         prompt_template = PromptTemplate.from_template(self.prompt_template)
         prompt_data = {
-            'requirement': self.requirement,
-            'exam_text': self.exam_text
+            'requirement': requirement,
+            'exam_text': exam_text
         }
-        self.prompt = prompt_template.invoke(prompt_data).to_string()
-        return
-    
-    
-    def _load_doc(self, file_path: str) -> str:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            req_text = file.read()
-            return req_text
+        prompt = prompt_template.invoke(prompt_data).to_string()
+        self.prompt = prompt
         return
 
 
-class AnswerParser:
-    def extract_json_from_string(text: str) -> dict:
-        match = re.search(r'```json\s*(\{.*?\})\s*```', text, re.DOTALL)
-        if not match:
-            raise ValueError('No JSON blocks found')
-        json_str = match.group(1)
-        
-        try:
-            return json.loads(json_str)
-        except json.JSONDecodeError as e:
-            raise ValueError(f'Error while decoding JSON: {e}') from e
+exam_sample = '6) Karl Marx dizia que a história humana pode ser entendida como uma luta entre classes ou grupos, ou setores, ou formas de produção. Isso significa:\na) Conflitos de classes.\nb) Filosofia da linguagem.\nc) Expansão marítima.\nd) Desenvolvimento da metafísica.'
+requirement = Requirement(file_path='knowledge/requirements/RF02.md')
+blamer = Blamer(prompt_template_path='knowledge/prompts/requirement_analysis.md', model='gemma3:4b')
+blamer.blame(exam_text=exam_sample, requirement=requirement)
