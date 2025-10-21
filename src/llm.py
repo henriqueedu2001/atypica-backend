4a5657f4f6a2238ec3d5a42dfab78df633189444
hsouza2001 <henrique.eduardo@virtualisurg.com>
2025-10-21 19:42:02 -0300

    feat: improve LLM class with JSON and prompts related methods

--- src/llm.py
+++ src/llm.py
@@ -1,3 +1,6 @@
+import json
+import re
+from pathlib import Path
 import ollama
 
 class LLM:
@@ -9,3 +12,19 @@ class LLM:
         answer = ollama.generate(model=self.model, prompt=query)
         answer = answer.get('response')
         return answer
+    
+    
+    def load_prompt(prompt_template_path: Path):
+        return
+    
+    
+    def extract_json_from_answer(text: str) -> dict:
+        match = re.search(r'```json\s*(\{.*?\})\s*```', text, re.DOTALL)
+        if not match:
+            raise ValueError('No JSON blocks found')
+        json_str = match.group(1)
+        
+        try:
+            return json.loads(json_str)
+        except json.JSONDecodeError as e:
+            raise ValueError(f'Error while decoding JSON: {e}') from e
