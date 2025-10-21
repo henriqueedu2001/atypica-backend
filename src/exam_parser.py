98e4890a5647868978186edaca31d679e1f06e3f
hsouza2001 <henrique.eduardo@virtualisurg.com>
2025-10-21 19:42:26 -0300

    chore: remove debug prints

--- src/exam_parser.py
+++ src/exam_parser.py
@@ -3,6 +3,7 @@ from file_manager import FileManager
 from exam import Exam
 from langchain_core.prompts import PromptTemplate
 from pathlib import Path
+from typing import *
 
 class ExamParser:
     def __init__(self, prompt_template_path: Path, model: str):
@@ -19,25 +20,17 @@ class ExamParser:
         return
     
     
-    def compile_prompt(self, exam_text: str):
+    def compile_prompt(self, exam_text: Union[str, Exam]):
         prompt_template = PromptTemplate.from_template(self.prompt_template)
         prompt_data = { 'exam_text': exam_text }
         prompt = prompt_template.invoke(prompt_data).to_string()
         self.prompt = prompt
-        print(prompt)
         return
     
     
-    def parse(self, exam_text: str):
+    def parse(self, exam_text: Union[str, Exam]):
         self.compile_prompt(exam_text=exam_text)
         llm = LLM(model=self.model)
         answer = llm.ask(self.prompt)
-        print(answer)
         answer = LLM.extract_json_from_answer(answer)
         return answer
-
-
-exam = Exam(file_path='files/PROVA_FIC_05.pdf')
-exam_parser = ExamParser(prompt_template_path='knowledge/prompts/exam_parsing.md', model='gemma3:12b')
-parsed_exam = exam_parser.parse(exam_text=exam.raw_text)
-FileManager.save_json('artifacts/fff.json', parsed_exam)
